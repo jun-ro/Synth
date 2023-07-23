d8253549df004c6ad4fbf93a272d500ab75ea91a
jun 3 <97373683+jun-ro@users.noreply.github.com>
2023-07-23 17:28:44 -0400

    Part of the new 'Event System'

--- Synth/EventHandling/ServerHandling.lua
+++ Synth/EventHandling/ServerHandling.lua
@@ -2,94 +2,42 @@
 -- Author: Junnie#0072
 -- License: MIT
 
-local EventHandler = require(script.Parent.EventHandling)
 
 local ServerHandler = {}
 
-function ServerHandler.new(proxy, config)
+function ServerHandler.new(config: {})
 	local self = setmetatable({}, {__index = ServerHandler})
-	self.proxy = proxy or nil
 	self.config = config
 	return self
 end
 
-function ServerHandler:Init()
-	local eventFolder = game:GetService("ReplicatedStorage"):FindFirstChild(self.config['Endpoint'].Value)
-	if eventFolder then
-		return
-	else
-		eventFolder = Instance.new('Folder', game:GetService("ReplicatedStorage"))
-		eventFolder.Name = self.config['Endpoint'].Value
-	end
+function ServerHandler:SetEvent(Event)
+	self.config.Proxy = Event
 end
 
-function ServerHandler:Intercept()
-	self.proxy.OnServerEvent:Connect(function(player, eventName, remove)
-		local endPointsFolder = game:GetService("ReplicatedStorage"):FindFirstChild("Endpoints")
-		if remove then
-			if not endPointsFolder then
-				endPointsFolder = Instance.new("Folder", game:GetService("ReplicatedStorage"))
-				endPointsFolder.Name = "Endpoints"
-			end
+function ServerHandler:Setup()
+	local EndpointFolder = self.config.Parent:FindFirstChild(self.config.Name)
 
-			local existingEvent = endPointsFolder:FindFirstChild(eventName)
-			if existingEvent then
-				error("Event name already used.")
-			else
-				local newEvent = Instance.new("RemoteEvent")
-				newEvent.Name = eventName
-				newEvent.Parent = endPointsFolder
-			end
+	if not EndpointFolder then
+		EndpointFolder = Instance.new('Folder', self.config.Parent)
+		EndpointFolder.Name = self.config.Name
+	end	
+end
+
+function ServerHandler:Hook()	
+	self.config.Proxy.OnServerEvent:Connect(function(player: Player, id: string, remove: boolean, functionName: string)
+		local EndpointFolder = self.config.Parent:FindFirstChild(self.config.Name)
+		if not remove then
+			local UniqueEvent = Instance.new('RemoteEvent')
+			UniqueEvent.Parent = EndpointFolder
+			UniqueEvent.Name = id
+			local funcPassed = self.config.EventsList[functionName]
+			UniqueEvent.OnServerEvent:Connect(funcPassed)
 		else
-			endPointsFolder:FindFirstChild(eventName):Destroy()
+			EndpointFolder:FindFirstChild(tostring(id)):Destroy()
 		end
 	end)
 end
-
-function ServerHandler.GetEvents()
-	local eventPass = EventHandler.new(game.ReplicatedStorage.Proxy.ProxyEvent)
-	return eventPass:GetEvents()
-end
-
-function ServerHandler.InterceptAll(eventsList, functionsList)
-	for i, v in ipairs(eventsList) do
-		local eventName = v.Name
-		local eventHandler = functionsList[eventName]
-
-		if eventHandler then
-			local eventConnection = v.OnServerEvent:Connect(eventHandler)
-
-			v.AncestryChanged:Connect(function(child, parent)
-				if parent == nil then
-					eventConnection:Disconnect()
-				end
-			end)
-		else
-			error("No function defined for event: " .. eventName)
-		end
-	end
-end
-
-function ServerHandler:Route(eventName, func_)
-	local APIFolder = game.ReplicatedStorage:WaitForChild(self.config:WaitForChild('Endpoint').Value)
-	
-	print(self.config:WaitForChild('Endpoint').Value)
-	
-	if APIFolder then
-		APIFolder = game:GetService("ReplicatedStorage")[self.config['Endpoint'].Value]
-	else
-		print("Idk what happened, api folder not working 🤔")
-	end
-	
-	local eventConnection = APIFolder:WaitForChild(eventName).OnServerEvent:Connect(func_)
-	
-	APIFolder:WaitForChild(eventName).AncestryChanged:Connect(function(child, parent)
-		if parent == nil then
-			eventConnection:Disconnect()
-		end
-	end)
-end
-
 
 
 return ServerHandler
